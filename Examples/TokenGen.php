911ab0fa487891e683f1f0322ec5173a806f2af4
Henrik Karapetyan <henrikkarapetyan@gmail.com>
2019-08-09 15:39:10 +0400

    1

--- Examples/TokenGen.php
+++ Examples/TokenGen.php
@@ -3,7 +3,7 @@
 
 require "../vendor/autoload.php";
 const SIGNATURE_PRIVATE_KEY = "secret1sdfsfsdfsdfsdf";
-const TOKEN_PRIVATE_IV = "secret-secret123";
+const TOKEN_PRIVATE_IV = "secret-secret123"; // this must contain 16 symbols.
 const TOKEN_PRIVATE_KEY = "secret3sdfsdfsdfsdfsdfsdf";
 
 $start = microtime(true);
@@ -14,18 +14,18 @@ for ($i = 0; $i < 10000; $i++) {
         TOKEN_PRIVATE_KEY,
         TOKEN_PRIVATE_IV,
         SIGNATURE_PRIVATE_KEY,
-        \HashAuth\Algorithms::ENCRYPT_AES_256_XTS,
-        \HashAuth\Algorithms::HASH_SHA256
+        \HashAuth\Algorithms::ENCRYPT_AES_256_CTR,
+        \HashAuth\Algorithms::HASH_SHA512
     );
 
     $token = $tokenManager->makeToken([
         'id' => 1,
         'name' => "Demo",
         'last_name' => "Demo"
-    ], ['sessId' => rand(1, 100000)]);
-    var_dump($i, $token);
+    ], ['sessId' => rand(1, 10000000)]);
+    echo $i.$token."\n";
 //    $data = $tokenManager->parseToken($token, []);
 //    var_dump($i, json_encode($data), $token);
 }
 
-var_dump("elapsed time",$time_elapsed_secs = microtime(true) - $start);
+echo "elapsed time".$time_elapsed_secs = microtime(true) - $start;
